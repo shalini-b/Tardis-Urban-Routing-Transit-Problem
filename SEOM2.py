a75cd476b22c4c8a201105a480ab8f5295db9ec5
Shalini Bhaskara <shalini.bhaskara91@gmail.com>
2018-05-02 12:44:57 -0400

    Add_nodes and Pick_best by SriNikhil Reddy

--- SEOM2.py
+++ SEOM2.py
@@ -44,23 +44,65 @@ class RouteSet(object):
             self.routes.append(route)
 
             route_reversed = False
-            while len(route.path_nodes) < route_len and not route_reversed:
+            while len(route.path_nodes) < route_len:
                 next_nodes = route.fetch_next_nodes(cur_node)
                 if not next_nodes:
                     if not route_reversed:
                         route.reverse_route()
                         route_reversed = True
                     else:
+                        # Adding at both ends tested, leave
                         break
                 else:
                     next_node = random.choice(next_nodes)
-                    route.path_nodes.append(next_node)
+                    # TODO: if we need sequential path nodes even
+                    # after reversing, we need reverse path_nodes list too
+                    route.append_to_path_end(next_node)
                     self.chosen.append(next_node.id)
                     cur_node = next_node
 
         if len(self.chosen) < self.num_nodes:
             res = self.repair()
             return self.routes if res else []
+
+    def add_nodes(self):
+        # Choose a random number of nodes to add
+        count = random.randrange(1, (self.max_route_len * self.num_routes) // 2)
+
+        routes_checked = []
+        while count > 0:
+            # Check if all routes are exhausted for addition
+            if len(routes_checked) == self.num_routes:
+                break
+
+            # Select the route not checked till now
+            rand_route = self.routes[random.randrange(self.num_routes)]
+            while rand_route in routes_checked:
+                rand_route = self.routes[random.randrange(self.num_routes)]
+
+            # Append it to routes checked
+            routes_checked.append(rand_route)
+
+            # Add nodes at the end/start of the route if possible
+            route_reversed = False
+            # Checking if the length of route is lesser than
+            # max_route_len mentioned by user
+            while len(rand_route.path_nodes) < self.max_route_len:
+                next_nodes = rand_route.fetch_next_nodes(rand_route.end)
+                if not next_nodes:
+                    # Reverse the route & try adding at the start
+                    if not route_reversed:
+                        rand_route.reverse_route()
+                        route_reversed = True
+                    else:
+                        # Adding at both ends tested, leave
+                        break
+                else:
+                    next_node = random.choice(next_nodes)
+                    # TODO: if we need sequential path nodes even
+                    # after reversing, we need reverse path_nodes list too
+                    rand_route.append_to_path_end(next_node)
+                    self.chosen.append(next_node.id)
 
     def repair(self):
         return
@@ -83,6 +125,10 @@ class Route(object):
     def reverse_route(self):
         # TODO: do we have to reverse path too?
         self.start, self.end = self.end, self.start
+
+    def append_to_path_end(self, node):
+        self.path_nodes.append(node)
+        self.end = node
 
 
 class TransitGraph(object):
@@ -114,6 +160,28 @@ class TransitGraph(object):
                     self.num_nodes)
             )
 
+    def pick_best(self, parent, offspring):
+        best_val = 0
+        target = None
+
+        # Finding the best route in parent wrt offspring
+        for route in parent.routes:
+            # Find the number of common nodes between the
+            # selected route of parent and the offspring routeset
+            common_nodes_len = len(set(list(map(lambda x: x.id, route.path_nodes)) + offspring.chosen))
+
+            if common_nodes_len > 1:
+                route_len = len(route.path_nodes)
+                target_len = (route_len - common_nodes_len)/route_len
+                if target_len > best_val:
+                    target = route
+                    best_val = target_len
+
+        return target
+
+    def crossover(self, parent1, parent2):
+        pass
+
 
 if __name__ == '__main__':
     # List of node objects
@@ -124,7 +192,8 @@ if __name__ == '__main__':
         for id, coords in enumerate(f.readlines()):
             posx, posy = list(map(float, coords.split()))
             # Populate list of nodes
-            nodes.append(Node(id, posx, posy))
+            # NOTE: id of nodes is different from the one listed in coords file
+            nodes.append(Node(id + 1, posx, posy))
 
     # Travel time defined from src to dest if applicable, 0 otherwise
     travel_times = [[0] * size] * size
